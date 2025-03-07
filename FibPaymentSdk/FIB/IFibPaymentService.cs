d6f8f937ad0a4d92f60cc85b62a5dd921ced6a9c
pshtiwan-hassan <67946816+pshtiwan-hassan@users.noreply.github.com>
2025-03-07 13:46:07 +0300

    Add files via upload

---
+++ FibPaymentSdk/FIB/IFibPaymentService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using FibPaymentSdk.FIB.Dtos;
+
+namespace FibPaymentSdk.FIB
+{
+    public interface IFibPaymentService
+    {
+        
+        Task<FibAuthorizationResponse> GetAccessTokenAsync();
+
+        
+        Task<FibCreatePaymentResponse?> CreatePaymentAsync(FibCreatePaymentRequest request);
+
+         
+        Task<FibPaymentStatusResponse?> CheckPaymentStatusAsync(string paymentId); 
+        Task CancelPaymentAsync(string? paymentId);
+        Task RefundPaymentAsync(string? paymentId);
+    }
+}
